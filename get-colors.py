71e9245aa4c6f8bbde06a0b6384274dd2be46037
crnbrdrck <broderickciaran@gmail.com>
2018-10-02 18:20:08 +0100

    Adding a script to get Github colours from the linguist repo

---
+++ get-colors.py
@@ -0,0 +1,20 @@
+# Cloned for https://github.com/doda/github-language-colors
+import subprocess
+import yaml
+import json
+import os
+
+replace_names = {
+    'C++': 'cpp',
+    'C#': 'C Sharp'
+}
+
+subprocess.call(['wget', 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml', '-q'])
+
+with open('languages.yml') as f:
+    colors = yaml.load(f)
+os.remove('languages.yml')
+colors = dict((replace_names.get(name, name), color['color']) for name, color in colors.items() if 'color' in color)
+
+with open('dist/colors.json', 'w') as f:
+    json.dump(colors, f)
