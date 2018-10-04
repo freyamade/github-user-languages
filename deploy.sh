8593c1a69e9869a271f6bdf984b77e511a01b525
crnbrdrck <broderickciaran@gmail.com>
2018-10-04 15:19:43 +0100

    Moved the entire deploy job to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
+# Write message stating which version is being deployed
+echo "Deploying github-user-languages $(jq '.version' package.json) to the Web Store!"
+
+# Build the production code
+npm run build
+
+# Create the distribution zipfile
+zip -r dist.zip
+
 # Deploy to Chrome Web Store
 ACCESS_TOKEN=$(curl "https://accounts.google.com/o/oauth2/token" -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&refresh_token=${REFRESH_TOKEN}&grant_type=refresh_token&redirect_uri=urn:ietf:wg:oauth:2.0:oob" | jq -r .access_token)
 curl -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-goog-api-version: 2" -X PUT -T dist.zip -v "https://www.googleapis.com/upload/chromewebstore/v1.1/items/${APP_ID}"
