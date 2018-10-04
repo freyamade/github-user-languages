16764cac410da020ea216b40cd78c769d1d1d527
crnbrdrck <broderickciaran@gmail.com>
2018-10-04 12:43:46 +0100

    Removed the name replacement from the get-colors script as it breaks the coloring of C++ and C#

--- get-colors.py
+++ get-colors.py
@@ -4,17 +4,12 @@ import yaml
 import json
 import os
 
-replace_names = {
-    'C++': 'cpp',
-    'C#': 'C Sharp'
-}
-
 subprocess.call(['wget', 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml', '-q'])
 
 with open('languages.yml') as f:
-    colors = yaml.load(f)
+    langs = yaml.load(f)
 os.remove('languages.yml')
-colors = dict((replace_names.get(name, name), color['color']) for name, color in colors.items() if 'color' in color)
+colors = {name: lang['color'] for name, lang in langs.items() if 'color' in lang}
 
 with open('dist/colors.json', 'w') as f:
     json.dump(colors, f)
